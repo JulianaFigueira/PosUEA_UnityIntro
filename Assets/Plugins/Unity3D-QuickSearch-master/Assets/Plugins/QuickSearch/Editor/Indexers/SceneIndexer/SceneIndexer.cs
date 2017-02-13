5eb48de5c8d3485ca4969dced716e75aa985a01b
Juliana Figueira <juli.figueira@gmail.com>
2017-02-12 20:25:35 -0400

    Second commit, review code

---
+++ Assets/Plugins/Unity3D-QuickSearch-master/Assets/Plugins/QuickSearch/Editor/Indexers/SceneIndexer/SceneIndexer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace QuickSearch {
+
+	public sealed class SceneIndexer : SearchIndexerBase {
+		private readonly List<ISearchableElement> elements_ = new List<ISearchableElement>(100);
+
+		private readonly List<Transform> tempTransforms_ = new List<Transform>(100);
+
+		protected override void OnOpen () {
+			elements_.Clear();
+
+			var rootObjs = GetSceneRoots();
+			for (var i = 0; i < rootObjs.Count; ++i) {
+				var obj = rootObjs[i];
+				obj.GetComponentsInChildren<Transform>(true, tempTransforms_);
+
+				for (var j = 0; j < tempTransforms_.Count; ++j) {
+					var tr = tempTransforms_[j];
+					if (tr == null)
+						continue;
+
+					var element = new SceneSearchableElement(tr.gameObject);
+					elements_.Add(element);
+				}
+			}
+		}
+
+		private static List<GameObject> GetSceneRoots () {
+			var prop = new HierarchyProperty(HierarchyType.GameObjects);
+			var expanded = new int[0];
+			var roots = new List<GameObject>();
+
+			while (prop.Next(expanded)) {
+				var go = prop.pptrValue as GameObject;
+				if (go == null)
+					continue;
+				roots.Add(go);
+			}
+
+			return roots;
+		}
+
+		protected override List<ISearchableElement> GetElements () {
+			return elements_;
+		}
+	}
+}
